c173bcf53c5e88a21cbd7032da52cea683cde5ed
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-11 17:38:53 +0530

    script update

--- cypress/e2e/Cambaytiger/prod/Search.js
+++ cypress/e2e/Cambaytiger/prod/Search.js
@@ -17,7 +17,7 @@ describe('Search functionality', () => {
     cy.get('.AdressCont__inside > :nth-child(1) > div').click();
     cy.wait(5000);
 
-    cy.xpath("//div[@class='showOnDesktop']//input[@placeholder='Search for seafood, chicken & more']").type("But");
+    cy.get("div[class='showOnDesktop'] input[placeholder='Search for seafood, chicken & more']").type("But");
     cy.get("body > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > a:nth-child(1) > p:nth-child(1)").click();
     cy.get("div[class='showOnDesktop'] li[class='breadcrumbs__active'] a").should('contain', "Mutton Curry Cut");
 
