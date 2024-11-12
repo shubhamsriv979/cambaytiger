5d5b9bdcb2929604e299e0d3f36f0e138aaa8105
shubhamsriv979 <shubham.s@brimo.in>
2024-11-13 00:01:35 +0530

    Update Search.js

--- cypress/e2e/Plixlife/prod/Search.js
+++ cypress/e2e/Plixlife/prod/Search.js
@@ -9,17 +9,20 @@ describe('Search functionality check', () => {
 
   it('Search functionality check', () => {
     cy.visit('https://www.plixlife.com/');
-    // cy.frameLoaded("[id='wiz-iframe-intent']");
-    //      // Step 3: Interact with elements inside the iframe
-    //      cy.iframe("[id='wiz-iframe-intent']").within(() => {
-    //       cy.get("#ct-content-wrapper > span",{timeout:5000}).click();
-    //   });  
-    // // Search the product
-    // cy.get("li[class='plixlife-main-menu__searchInput'] div div[class='SearchSuggestionBar_wrapper__1Uc5K']").type("ACV");
-    // // Click on the product
-    // cy.get(':nth-child(1) > .productCardContainer > .sc-fYiAbW > .undefined__cardInfo > a > .productCard__name').click();
-    // // Verify the product title in PDP  
-    // cy.get('[data-test="productName"]').should('have.contain', 'Apple Cider Vinegar Effervescent with mother for weight loss');
+
+    // Access the iframe and wait for it to load
+    cy.get("#wiz-iframe-intent").then((iframedata) => {
+      // Access the iframe's document
+      iframedata.contents().find('body')      
+    })
+    cy.reload();
+
+    // Search the product
+    cy.get("li[class='plixlife-main-menu__searchInput'] div div[class='SearchSuggestionBar_wrapper__1Uc5K']").type("ACV");
+    // Click on the product
+    cy.get(':nth-child(1) > .productCardContainer > .sc-fYiAbW > .undefined__cardInfo > a > .productCard__name').click();
+    // Verify the product title in PDP  
+    cy.get('[data-test="productName"]').should('have.contain', 'Apple Cider Vinegar Effervescent with mother for weight loss');
   })
 
 })
