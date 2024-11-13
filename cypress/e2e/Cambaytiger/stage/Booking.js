2d69569cf00d8aede268ee286a18015268aeb2be
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-13 22:53:31 +0530

    Update Booking.js

--- cypress/e2e/Cambaytiger/stage/Booking.js
+++ cypress/e2e/Cambaytiger/stage/Booking.js
@@ -33,7 +33,7 @@ describe('Booking flow', () => {
     // cy.reload();
 
     // select location 
-    cy.get(':nth-child(1) > #header > .scss_mainNavContainerWrapper__m_O_A > .scss_mainNavContainer__UDVhL > .scss_logoSearchContainer__ca6MR > :nth-child(3) > .scss_GGLocation__cfABD > .scss_GGLocation__topCont__oRucC > :nth-child(5) > .GGLocation__input > input').type("Bangalore", { delay: 100, force: true });
+    cy.get('test:nth-child(1) > #header > .scss_mainNavContainerWrapper__m_O_A > .scss_mainNavContainer__UDVhL > .scss_logoSearchContainer__ca6MR > :nth-child(3) > .scss_GGLocation__cfABD > .scss_GGLocation__topCont__oRucC > :nth-child(5) > .GGLocation__input > input').type("Bangalore", { delay: 100, force: true });
     cy.get('.AdressCont__inside > :nth-child(1) > div').click();
 
     // stage Login
