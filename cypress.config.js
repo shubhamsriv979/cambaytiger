22aa88179f87710c58f71f9e6010629983fed3fe
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-14 01:06:16 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -16,19 +16,19 @@ module.exports = defineConfig({
     viewportHeight: 660,   // Set height here
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', // Location of your test files
     setupNodeEvents(on, config) {
-      // require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-mochawesome-reporter/plugin')(on);
       // Add custom events or plugins here, if needed
     },
   },
 
   video: true, // Disable video recording
 
-  // reporter: 'cypress-mochawesome-reporter', // Use the Mochawesome reporter
-  // reporterOptions: {
-  //   reportDir: 'cypress/reports', // Directory for the reports
-  //   charts: true, // Display charts in the report
-  //   reportPageTitle: 'My Test Suite', // Set a custom page title for the report
-  //   embeddedScreenshots: true, // Embed screenshots in the report
-  //   inlineAssets: true, // Use inline assets for the report
-  // }
+  reporter: 'cypress-mochawesome-reporter', // Use the Mochawesome reporter
+  reporterOptions: {
+    reportDir: 'cypress/reports', // Directory for the reports
+    charts: true, // Display charts in the report
+    reportPageTitle: 'My Test Suite', // Set a custom page title for the report
+    embeddedScreenshots: true, // Embed screenshots in the report
+    inlineAssets: true, // Use inline assets for the report
+  }
 });
