9b50d62a1e4f6a1195c4dd52546793aafa262e8a
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-21 13:30:19 +0530

    Update Membership.js

--- cypress/support/PageObjects/Membership.js
+++ cypress/support/PageObjects/Membership.js
@@ -19,9 +19,10 @@ class Membership {
         cy.wait(5000);
         cy.get('.AdressCont__inside > :nth-child(1) > div').click();
         cy.wait(5000); // Wait for the next steps to load
-        cy.get("#header > div.scss_mainNavContainerWrapper__m_O_A > div > div:nth-child(3) > ul > div.GG_dropDown_button.sc-hAZvFp.wBSEq > span").click();
-        cy.get("body > div:nth-child(1) > div:nth-child(1) > header:nth-child(1) > div:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > ul:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(2) > div:nth-child(1) > div:nth-child(2)").click();
-        cy.get('#__next > div > div.showOnDesktop > div > div.Membership_parentMemberContainer__Hbxf8 > div.Membership_memberPlanContainer__A_mY2 > div.Membership_activeMemberColumn__4tiu4 > button').click();
+        // cy.get("#header > div.scss_mainNavContainerWrapper__m_O_A > div > div:nth-child(3) > ul > div.GG_dropDown_button.sc-hAZvFp.wBSEq > span").click();
+        // cy.get("body > div:nth-child(1) > div:nth-child(1) > header:nth-child(1) > div:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > ul:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(2) > div:nth-child(1) > div:nth-child(2)").click();
+        // cy.get('#__next > div > div.showOnDesktop > div > div.Membership_parentMemberContainer__Hbxf8 > div.Membership_memberPlanContainer__A_mY2 > div.Membership_activeMemberColumn__4tiu4 > button').click();
+        cy.visit("https://farzistore-nh.farziengineer.co/page/membership");
     }
 
     closeAdvPopup() {
