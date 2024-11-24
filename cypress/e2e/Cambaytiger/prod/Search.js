caaeb9e69c798d905aa35d998cecbe053d7e0107
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-24 13:11:43 +0530

    Update Search.js

--- cypress/e2e/Cambaytiger/prod/Search.js
+++ cypress/e2e/Cambaytiger/prod/Search.js
@@ -10,7 +10,7 @@ describe('Search functionality', () => {
 
 
   it('Search functionality', () => {
-    cy.visit('https://cambaytigerstage-nh.farziengineer.co/');
+    cy.visit('https://cambaytiger.com/');
     // Access the iframe and wait for it to load (e.g., advertisement pop-up)
     Membership.closeAdvPopup();
 
