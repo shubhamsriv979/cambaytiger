4a4c7380c550bb28353a926d92cf740bdd0898fb
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-24 13:34:30 +0530

    Update Search.js

--- cypress/e2e/Cambaytiger/prod/Search.js
+++ cypress/e2e/Cambaytiger/prod/Search.js
@@ -42,30 +42,31 @@ describe('Search functionality', () => {
     cy.get(".our-categories__heading").should('be.visible');
 
 
-    const cartQuantitySelector = ".GG-main-menu__cart__quantity__gg";
-    const cartHeaderSelector = ".overlayFarzicom__header__text";
-    const deleteButtonSelector = ".sc-fsGQkc.bgexUZ";
-
-    if (
-      $body.find(cartQuantitySelector).is(':visible') &&
-      $body.find(cartQuantitySelector).text().trim() !== '0'
-    ) {
-      cy.log('Cart is not empty, proceeding to delete items.');
-
-      // Click on the Cart button
-      cy.contains("Cart").eq(0).click();
-
-      // Validate Cart heading
-      cy.get(cartHeaderSelector).should('exist').and('be.visible');
-
-      // Perform the delete action
-      cy.get(deleteButtonSelector).should('exist').click({ multiple: true });
-      cy.log('All items were deleted successfully.');
-    } else {
-      cy.log('Cart is empty, no action performed.');
-    }
-
-
+    cy.get('body').then(($body) => {
+      const cartQuantitySelector = ".GG-main-menu__cart__quantity__gg";
+      const cartHeaderSelector = ".overlayFarzicom__header__text";
+      const deleteButtonSelector = ".sc-fsGQkc.bgexUZ";
+    
+      if (
+        $body.find(cartQuantitySelector).is(':visible') &&
+        $body.find(cartQuantitySelector).text().trim() !== '0'
+      ) {
+        cy.log('Cart is not empty, proceeding to delete items.');
+    
+        // Click on the Cart button
+        cy.contains("Cart").eq(0).click();
+    
+        // Validate Cart heading
+        cy.get(cartHeaderSelector).should('exist').and('be.visible');
+    
+        // Perform the delete action
+        cy.get(deleteButtonSelector).should('exist').click({ multiple: true });
+        cy.log('All items were deleted successfully.');
+      } else {
+        cy.log('Cart is empty, no action performed.');
+      }
+    });
+    
 
 
 
