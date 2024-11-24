f32a642c6dd742fc4082272e98b977e85540281c
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-24 11:53:47 +0530

    cart

--- cypress/e2e/Cambaytiger/prod/Search.js
+++ cypress/e2e/Cambaytiger/prod/Search.js
@@ -23,54 +23,56 @@ describe('Search functionality', () => {
     cy.get('.AdressCont__inside > :nth-child(1) > div').click();
     cy.wait(10000);
 
-    cy.get("div[class='showOnDesktop'] input[placeholder='Search for seafood, chicken & more']").should('be.visible').type("But");
-    cy.get("body > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > a:nth-child(1) > p:nth-child(1)").click();
-    cy.get("div[class='showOnDesktop'] li[class='breadcrumbs__active'] a").should("be.visible");
+    // cy.get("div[class='showOnDesktop'] input[placeholder='Search for seafood, chicken & more']").should('be.visible').type("But");
+    // cy.get("body > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > a:nth-child(1) > p:nth-child(1)").click();
+    // cy.get("div[class='showOnDesktop'] li[class='breadcrumbs__active'] a").should("be.visible");
 
 
-    // // stage Login
-    // // cy.get("div[class='showOnDesktop'] nav[id='header'] div[class='scss_mainNavContainerWrapper__m_O_A'] div[class='scss_mainNavContainer__UDVhL'] div div[class='GG_dropDown_button sc-kZUnxY dAzJsv'] span").click();
-    // // cy.get("div[class='showOnDesktop'] nav[id='header'] div[class='scss_mainNavContainerWrapper__m_O_A'] div[class='scss_mainNavContainer__UDVhL'] div button[class='user-register']").click();
-    // // prod login
-    // cy.get("div[class='showOnDesktop'] nav[id='header'] div[class='scss_mainNavContainerWrapper__m_O_A'] div[class='scss_mainNavContainer__UDVhL'] div div[class='GG_dropDown_button sc-bryTEL fIJmHu'] span",).click();
-    // cy.get("div[class='showOnDesktop'] nav[id='header'] div[class='scss_mainNavContainerWrapper__m_O_A'] div[class='scss_mainNavContainer__UDVhL'] div button[class='user-register']").click();
-
-    // cy.wait(5000);
-    // cy.get("input[placeholder='Enter Phone number']").click().type("6388789049", { delay: 100, force: true });
-    // cy.get('.scss_loginRegCont__m2Dae > button').click();
-    // cy.get("#otp-0").type("123456",);
-    // cy.get('.scss_loginRegCont__m2Dae > button').click();
-    // cy.wait(5000);
-
-    // cy.contains("Cart").eq(0).click();
-    // //cart heading text
-    // cy.get(".overlayFarzicom__header__text").should('be.visible');
-    // cy.wait(10000);    
-
-    // function clickUntilVisible() {
-    //   // Use recursion safely
-    //   cy.get('body').then(($body) => {
-    //     // Check if the target element is visible
-    //     cy.wait(10000); 
-    //     if ($body.find("div[class='cart__empty'] span").is(':visible')) {
-    //       cy.log('Target element is now visible.');
-          
-    //       // Perform the click action
-    //       cy.get(".sc-fsGQkc.bgexUZ").click();
-          
-    //       // Call the function again to check the condition
-    //       cy.wait(5000); // Add a small delay to prevent rapid execution
-    //       clickUntilVisible();
-    //     } else {
-    //       cy.log('Target element is not visible yet.');
-    //     }
-    //   });
-    // }
+    //  Login
+    cy.get(':nth-child(1) > #header > .scss_mainNavContainerWrapper__m_O_A > .scss_mainNavContainer__UDVhL > :nth-child(3) > .GG-main-menu__lower__desktop-right__ul > .GG_dropDown_button > span').click();
+    cy.get("div[class='showOnDesktop'] nav[id='header'] div[class='scss_mainNavContainerWrapper__m_O_A'] div[class='scss_mainNavContainer__UDVhL'] div button[class='user-register']").click();
     
-    // // Invoke the function
-    // clickUntilVisible();
+
+    cy.wait(5000);
+    cy.get("input[placeholder='Enter Phone number']").click().type("6388789049", { delay: 100, force: true });
+    cy.get('.scss_loginRegCont__m2Dae > button').click();
+    cy.get("#otp-0").type("123456",);
+    cy.get('.scss_loginRegCont__m2Dae > button').click();
+    cy.wait(5000);
+
+    cy.contains("Cart").eq(0).click();
+    //cart heading text
+    cy.get(".overlayFarzicom__header__text").should('be.visible');
+    cy.wait(10000);    
+
+    function clickUntilVisible() {
+      // Use recursion safely
+      cy.get('body').then(($body) => {
+        // Check if the target element is visible
+        cy.wait(10000); 
+        if ($body.find("div[class='cart__empty'] span").is(':visible')) {
+          cy.log('Cart is empty');          
+        }
+        else if ($body.find(".sc-fsGQkc.bgexUZ").is(':visible')) {         
+          
+          // Perform the click action
+          cy.get(".sc-fsGQkc.bgexUZ").click();
+          cy.log('Item is deleted');         
+          // Call the function again to check the condition
+          cy.wait(5000); // Add a small delay to prevent rapid execution
+          clickUntilVisible();
+        }
+        else {
+          cy.log('Both conditions failed');
+          clickUntilVisible();
+        }
+      });
+    }
     
-    // cy.get(".overlayFarzicom__header__close-icon").click();
+    // Invoke the function
+    clickUntilVisible();
+    
+    cy.get(".overlayFarzicom__header__close-icon").click();
 
 
   })
