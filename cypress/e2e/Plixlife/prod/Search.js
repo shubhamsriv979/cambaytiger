2cac86811f95a2a02a59da89f31b27453ce50b2b
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-26 10:09:28 +0530

    stage product urls

--- cypress/e2e/Plixlife/prod/Search.js
+++ cypress/e2e/Plixlife/prod/Search.js
@@ -1,30 +1,22 @@
 import 'cypress-iframe'
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // Prevent Cypress from failing the test on uncaught exceptions
+  // returning false here prevents Cypress from
+  // failing the test due to the uncaught exception
   return false;
 });
-
-beforeEach(() => {
-  cy.intercept('https://example.com/api/geo', {
-    body: { region: 'IN' }, // Mock the geo response as Indian
-  }).as('mockGeo');
-});
-
 
 describe('Search functionality check', () => {
 
   it('Search functionality check', () => {
     cy.visit('https://www.plixlife.com/');
-    
 
+    // Access the iframe and wait for it to load
+    cy.get("#wiz-iframe-intent").then((iframedata) => {
+      // Access the iframe's document
+      iframedata.contents().find('body')      
+    })
+    cy.reload();
 
-    // // Access the iframe and wait for it to load
-    // cy.get("#wiz-iframe-intent").then((iframedata) => {
-    //   // Access the iframe's document
-    //   iframedata.contents().find('body')      
-    // })
-    // cy.reload();
-cy.wait(20000);
     // Search the product
     cy.get("li[class='plixlife-main-menu__searchInput'] div div[class='SearchSuggestionBar_wrapper__1Uc5K']").type("ACV");
     // Click on the product
