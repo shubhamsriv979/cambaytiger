a43880fa060312dde2692cb32411d1e812e48930
Shubham-git779 <114850207+Shubham-git779@users.noreply.github.com>
2024-11-29 14:37:18 +0530

    Update Membership.js

--- cypress/support/PageObjects/Membership.js
+++ cypress/support/PageObjects/Membership.js
@@ -21,22 +21,22 @@ class Membership {
 
     closeAdvPopup() {
         cy.wait(10000);
-        // cy.get("#wzrkImageOnlyDiv").then((iframedata) => {
-        //     // Access the contents of the iframe's body
-        //     iframedata.contents().find('body');
-        // });
-        // function waitForElementAndClosePopup() {
-        //     cy.get('body').then((body) => {
-        //         if (body.find("#wzrkImageOnlyDiv").length > 0) {
-        //             // Element exists; access the iframe
-        //             cy.reload();
-        //             waitForElementAndClosePopup(); // Recursive call
-        //         }
-        //     });
-        // }
+        cy.get("#wzrkImageOnlyDiv").then((iframedata) => {
+            // Access the contents of the iframe's body
+            iframedata.contents().find('body');
+        });
+        function waitForElementAndClosePopup() {
+            cy.get('body').then((body) => {
+                if (body.find("#wzrkImageOnlyDiv").length > 0) {
+                    // Element exists; access the iframe
+                    cy.reload();
+                    waitForElementAndClosePopup(); // Recursive call
+                }
+            });
+        }
         cy.reload();
-        // // // Call the function in your test
-        // // waitForElementAndClosePopup();
+        // // Call the function in your test
+        waitForElementAndClosePopup();
     }
 
 
